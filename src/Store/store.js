1468483b5839e6c5480251ec8e858364ba0d03d2
KAMALAMAN687 <kamalaman687@gmail.com>
2023-09-09 19:57:52 +0530

    ADDED REDUX AND REDUX TOOLKIT FUNCTIONALITY

---
+++ src/Store/store.js
@@ -0,0 +1,11 @@
+//Reducer Imports.
+import todoReducer from "../slices/todoSlice";
+import { configureStore } from "@reduxjs/toolkit";
+const store = configureStore({
+  reducer: {
+    todo: todoReducer,
+  },
+  devTools: true,
+});
+
+export default store;
---
+++ src/components/AddTodo/AddTodo.jsx
@@ -0,0 +1,27 @@
+import { useContext, useState } from "react";
+import { useDispatch } from "react-redux";
+import { addTodo } from "../../Actions/todoActions";
+function AddTodo({ addTodo }) {
+  const [inputText, setInputText] = useState("");
+  const dispatch = useDispatch();
+  return (
+    <div>
+      <input
+        type="text"
+        placeholder="Add Your Next Todo"
+        value={inputText}
+        onChange={(e) => setInputText(e.target.value)}
+      />
+      <button
+        onClick={() => {
+          addTodo({ todoText: inputText });
+          setInputText("");
+        }}
+      >
+        Add{" "}
+      </button>
+    </div>
+  );
+}
+
+export default AddTodo;
